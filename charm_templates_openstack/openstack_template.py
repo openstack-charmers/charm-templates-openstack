a111a4299b3d2266296656486c7f8fb73590ce2b
Liam Young <liam.young@canonical.com>
2017-02-01 11:32:42 +0000

    General tidy

--- charm_templates_openstack/openstack_template.py
+++ charm_templates_openstack/openstack_template.py
@@ -15,118 +15,113 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import jinja2
 import logging
 import os
-import os.path as path
-import time
 import shutil
+import stat
 import subprocess
 import tempfile
+import time
 
-import stat
-import jinja2
-
-from charmtools.generators import (
-    CharmTemplate,
-    Prompt,
-    PromptList,
-)
+import charmtools.generators
 
 log = logging.getLogger(__name__)
 
 
-class OpenStackCharmTemplate(CharmTemplate):
+class OpenStackCharmTemplate(charmtools.generators.CharmTemplate):
 
     # _EXTRA_FILES is the list of names of files present in the git repo
     # we don't want transferred over to the charm template:
     _EXTRA_FILES = ["README.md", ".git", ".gitmodules"]
 
     def create_charm(self, config, output_dir):
+        """Generate charm from template
+
+        :param config: dict Base context for rendering templates
+        :param output_dir: str Directory to write charm out to
+        """
         self.config = config
-        self._clone_template(config, output_dir)
-        self.update_template_ctxt()
-        self.rename_files(output_dir)
-        for root, dirs, files in os.walk(output_dir):
+        self.output_dir = output_dir
+        self.clone_template_repo()
+        self.rename_files()
+        for root, dirs, files in os.walk(self.output_dir):
             for outfile in files:
-                if self.skip_template(outfile):
-                    continue
+                if not self.skip_template(outfile):
+                    self.write_template(os.path.join(root, outfile))
 
-                self._template_file(self.config, path.join(root, outfile))
+    @property
+    def template_context(self):
+        """Template context"""
+        self.config['charm_class'] = '{}Charm'.format(
+            self.config['metadata']['package'].title().replace('-', ''))
+        self.config['charm_lib'] = self.config['metadata']['package'].replace(
+            '-', '_')
+        return self.config
 
-    def update_template_ctxt(self):
-        charm_initcap = [a.title()
-                         for a
-                         in self.config['metadata']['package'].split('-')]
-        charm_class = '{}Charm'.format(''.join(charm_initcap))
-        self.config['charm_class'] = charm_class
-        new_name = self.config['metadata']['package'].replace('-', '_')
-        self.config['charm_lib'] = '{}'.format(new_name)
+    def write_template(self, outfile):
+        """Write out template using template_context, preserving file perms
 
-    def _template_file(self, config, outfile):
-        if path.islink(outfile):
+        :param outfile: File to render
+        """
+        if os.path.islink(outfile):
             return
-
-        # This is ugly. The template to use is known (its outfile) but there
-        # seems to be no way to point jinja directly at a template . Instead it
-        # wants to search for it. So extract the directory from outfile and
-        # point the jinja2 loader there.
-        charm_subdir = os.path.dirname(outfile)
-        loaders = [jinja2.FileSystemLoader(charm_subdir)]
-        jinja_env = jinja2.Environment(loader=jinja2.ChoiceLoader(loaders))
-        template = jinja_env.get_template(os.path.basename(outfile))
-        t = template.render(config)
         mode = os.stat(outfile)[stat.ST_MODE]
-        o = tempfile.NamedTemporaryFile(
-            dir=path.dirname(outfile), delete=False)
-        os.chmod(o.name, mode)
-        o.write(str(t))
-        o.close()
-        backupname = outfile + str(time.time())
-        os.rename(outfile, backupname)
-        os.rename(o.name, outfile)
-        os.unlink(backupname)
+        with open(outfile, 'r') as f:
+            template_src = f.read()
+        template = jinja2.Template(template_src)
+        with open(outfile, 'wb') as f:
+            os.chmod(outfile, mode)
+            f.write(template.render(self.template_context))
 
-    def _clone_template(self, config, output_dir):
-        templ_url =  os.environ.get(
-            'CHARM_TEMPLATE_LOCAL_BRANCH',
+    def clone_template_repo(self):
+        """Clone Git repository containing templated charm
+
+        """
+        templ_url = os.environ.get(
+            'CHARM_TEMPLATE_ALT_REPO',
             self._TEMPLATE_URL)
         cmd = "git clone --recursive {} {}".format(
-            templ_url, output_dir
+            templ_url, self.output_dir
         )
-        try:
-            subprocess.check_call(cmd.split())
-        except OSError as e:
-            raise Exception(
-                "The below error has occurred whilst attempting to clone"
-                "the charm template. Please make sure you have git"
-                "installed on your system.\n" + e
-            )
+        subprocess.check_call(cmd.split())
 
         # iterate and remove all the unwanted files from the git repo:
-        for item in [path.join(output_dir, i) for i in self._EXTRA_FILES]:
-            if not path.exists(item):
+        for item in [os.path.join(self.output_dir, i)
+                     for i in self._EXTRA_FILES]:
+            if not os.path.exists(item):
                 continue
-
-            if path.isdir(item) and not path.islink(item):
+            if os.path.isdir(item) and not os.path.islink(item):
                 shutil.rmtree(item)
             else:
                 os.remove(item)
 
     @property
-    def src_handler_file(self):
-        return '{}_charm_handlers.py'.format(self.charm_type)
+    def handler_dir(self):
+        """Directory containing reactive handler template"""
+        return os.path.join(self.output_dir, 'src', 'reactive')
 
     @property
-    def src_charm_file(self):
-        return '{}_charm.py'.format(self.charm_type)
+    def charm_lib_dir(self):
+        """Directory containing charm lib module"""
+        return os.path.join(self.output_dir, 'src', 'lib', 'charm',
+                            'openstack')
 
-    def rename_files(self, output_dir):
-        new_handler_name = '{}_handlers.py'.format(self.config['charm_lib'])
-        new_lib_name = '{}.py'.format(self.config['charm_lib'])
-        src_handler_dir = os.path.join(output_dir, 'src', 'reactive')
-        src_charm_lib_dir = os.path.join(output_dir, 'src', 'lib', 'charm',
-'openstack')
-        os.rename(os.path.join(src_handler_dir, self.src_handler_file),
-                  os.path.join(src_handler_dir, new_handler_name))
-        os.rename(os.path.join(src_charm_lib_dir, self.src_charm_file),
-                  os.path.join(src_charm_lib_dir, new_lib_name))
+    @property
+    def file_renames(self):
+        """Files that require renaming"""
+        return [
+            {'dirname': self.handler_dir,
+             'src_file': self.charm_type + '_charm_handlers.py',
+             'dest_file': self.template_context['charm_lib'] + '_handlers.py'},
+            {'dirname': self.charm_lib_dir,
+             'src_file': self.charm_type + '_charm.py',
+             'dest_file': self.template_context['charm_lib'] + '.py'}]
+
+    def rename_files(self):
+        """Rename files to charm specific names"""
+        for rename_info in self.file_renames:
+            pass
+            os.rename(
+                os.path.join(rename_info['dirname'], rename_info['src_file']),
+                os.path.join(rename_info['dirname'], rename_info['dest_file']))
